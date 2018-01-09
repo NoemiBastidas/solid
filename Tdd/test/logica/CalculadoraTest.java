d8cabefd7452eb3b14f26ba5c4ae2f301165db4a
gabosoam <gabrielsalazar@outlook.com>
2018-01-09 13:49:28 -0500

    test

---
+++ Tdd/test/logica/CalculadoraTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gabriel
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sumar method, of class Calculadora.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("sumar");
+        int primerNumero = 10;
+        int segundoNumero = 20;
+        Calculadora instance = new Calculadora();
+        int expResult = 30;
+        int result = instance.sumar(primerNumero, segundoNumero);
+        assertEquals(expResult, result);
+
+    }
+    
+}
